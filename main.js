a4874da4301153ffa1a045a2b11f502a28a12ece
liuhuimin0063 <2137568354@qq.com>
2018-05-09 18:13:30 +0800

    first cmmit

---
+++ main.js
@@ -0,0 +1,42 @@
+/*const fibonacci = require('./lib/fibonacci')
+for(var i=1;i<=20;i++){
+    console.log(fibonacci(i));
+}
+*/
+
+/*const fibonacci = require('./lib/fibonacci')
+var len=20;
+for(; len--; ){
+    console.log(fibonacci(len));
+}
+*/
+
+//console.log(fibonacci(1));
+//console.log("hello,world");
+
+
+
+/*function isLeapYear(n){
+    if(n % 400===0) {
+  return true
+     
+    }
+    if(n % 100===0) {
+  return false
+     
+    }
+    if(n % 4===0) {
+  return true 
+     
+    }
+    return false
+
+}
+
+
+console.log("2000:"+ isLeapYear(2000));
+console.log("1900:"+ isLeapYear(1900));
+console.log("2008:"+ isLeapYear(2008));
+console.log("2009:"+ isLeapYear(2009));
+
+*/
